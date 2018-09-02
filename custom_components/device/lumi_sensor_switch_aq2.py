419fc3719f8a9eba54e279bf9c04eac6315c0e6a
Yoda-x <Yoda-x>
2018-09-02 14:21:38 +0200

    new sensor

--- custom_components/device/lumi_sensor_switch_aq2.py
+++ custom_components/device/lumi_sensor_switch_aq2.py
@@ -2,10 +2,8 @@
 
 def _custom_endpoint_init(self, node_config, *argv):
     config = {
-         "in_cluster": [0x0000, ],
-         "out_cluster": [0x0006],
+        "in_cluster": [0x0000, 0x0006 ],
         "type": "binary_sensor",
         }
     node_config.update(config)
-    self.add_output_cluster(6)
 
